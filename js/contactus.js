41375fbd98fab307b36dc3be38a6a0049a1dc5e1
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-07-20 21:57:13 +0530

    js removed

--- js/contactus.js
+++ js/contactus.js
@@ -1,7 +1,3 @@
-document.getElementById('contactForm')
-function change() {
-    alert('Your message has been submitted!')
-};
 function msg() {
     var message = document.getElementById('message').value.trim(); 
     if (message === "empty") {
