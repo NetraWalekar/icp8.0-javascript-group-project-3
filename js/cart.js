7bc04a1d8cd811f8dda3a65df9d7141530eb6e18
nilparate <nilpatare7700@gmail.com>
2024-07-20 19:20:39 +0530

    changes page

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,8 @@
-let cartIcon = document.querySelector("#cart-icon");
+let cartIcon = document.querySelector("#categories");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
 // open cart
-cartIcon.onclick = () => {
+categories.onclick = () => {
     cart.classList.add("active");
 };
 //close cart
