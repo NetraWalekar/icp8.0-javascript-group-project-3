980dbbf5e63de2a157a7b1a0e1701db33285cad7
swaroopatarkase <tarkaseswaroopa@gmail.com>
2024-07-20 21:53:53 +0530

    more js added

--- js/contactus.js
+++ js/contactus.js
@@ -2,3 +2,11 @@ document.getElementById('contactForm')
 function change() {
     alert('Your message has been submitted!')
 };
+function msg() {
+    var message = document.getElementById('message').value.trim(); 
+    if (message === "empty") {
+        alert('Fill all Information'); 
+    } else {
+        alert('Message sent');
+    }
+}
