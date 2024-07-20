0de1c107caa2d2b4c1476824094a9a3790941648
NetraWalekar <netrawalekar6@gmail.com>
2024-07-21 00:11:55 +0530

    updates

--- js/categories.js
+++ js/categories.js
@@ -1,25 +1,25 @@
-function added(no){
+function added(no) {
     const btn = document.getElementById(no)
-    btn.style.backgroundColor="green"
+    btn.style.backgroundColor = "green"
+}
+
+function plusminus(action) {
+    const quantitytext = document.getElementById("quantity-text")
+    let quantity = parseInt(quantitytext.innerText);
+    if (quantity == 1 && action == 'decrement') {
+        alert('Quantity cannot be less than 1')
+        return
+    }
+    else if (quantity == 5 && action == 'increment') {
+        alert('Quantity cannot be greater then 5')
+        return
     }
 
-    function plusminus(action) {
-        const quantitytext = document.getElementById("quantity-text")
-        let quantity = parseInt(quantitytext.innerText);
-        if (quantity == 1 && action == 'decrement') {
-           alert('Quantity cannot be less than 1')
-           return
-        }
-        else if (quantity == 5 && action == 'increment') {
-           alert('Quantity cannot be greater then 5')
-           return
-        }
+    if (action == 'increment') {
+        quantitytext.innerText = ++quantity;
+    }
+    else {
+        quantitytext.innerText = --quantity;
+    }
 
-        if (action == 'increment') {
-           quantitytext.innerText = ++quantity;
-        }
-        else {
-           quantitytext.innerText = --quantity;
-        }
-
-     }
+}
