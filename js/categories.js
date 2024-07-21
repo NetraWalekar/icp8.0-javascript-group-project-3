3eaf7261d93d96bb3a560bb5359a38384cf6d459
NetraWalekar <netrawalekar6@gmail.com>
2024-07-21 11:30:06 +0530

    updates

--- js/categories.js
+++ js/categories.js
@@ -2,26 +2,28 @@ function added(no) {
     const btn = document.getElementById(no)
     btn.style.backgroundColor = "green"
 }
-
-function plusminus(action) {
-    const quantitytext = document.getElementById("quantity-text")
-    let quantity = parseInt(quantitytext.innerText);
-    if (quantity == 1 && action == 'decrement') {
-        alert('Quantity cannot be less than 1')
-        return
+function plusminus(action)
+{
+   const quantitytext =document.getElementById("quantity-text")
+   let quantity = parseInt(quantitytext.innerText);
+   if(quantity==1 && action=='decrement')
+   {
+    alert('Quantity cannot be less than 1')
+    return
+   }
+   else if (quantity==5 && action =='increment')
+   {
+     alert('Quantity cannot be greater then 5')
+     return
     }
-    else if (quantity == 5 && action == 'increment') {
-        alert('Quantity cannot be greater then 5')
-        return
-    }
-
-    if (action == 'increment') {
-        quantitytext.innerText = ++quantity;
-    }
-    else {
-        quantitytext.innerText = --quantity;
-    }
-
+   
+   if (action == 'increment'){
+    quantitytext.innerText =++quantity;
+   }
+   else{
+    quantitytext.innerText =--quantity;
+   }
+   
 }
 
 function add_cart(num){
