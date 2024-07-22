6be40a99f171546c844cb0e899e4e422d030dc52
madhumitasugandhi <madhumitasugandhi@gmail.com>
2024-07-22 08:09:01 +0530

    BillingCity added by id.

--- js/checkout.js
+++ js/checkout.js
@@ -40,6 +40,7 @@ document.getElementById('place-order-button').addEventListener('click', () => {
     const billingName = document.getElementById('billing-name').value;
     const billingNo = document.getElementById('billing-no').value;
     const billingAddress = document.getElementById('billing-address').value;
+    const billingCity = document.getElementById('billing-city').value;
    
    
     
