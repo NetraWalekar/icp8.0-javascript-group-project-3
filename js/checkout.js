34ea815209efe7fd558be73ea010b125035e1b2b
madhumitasugandhi <madhumitasugandhi@gmail.com>
2024-07-22 07:25:08 +0530

    Javascript function started

--- js/checkout.js
+++ js/checkout.js
@@ -5,3 +5,12 @@ const cartItems = [
     { name: 'Silk Cadbury', price: 100, quantity: 3 },
     { name: 'Cherry', price: 159, quantity: 1 }
 ];
+
+function updateOrderSummary() 
+{
+    const cartList = document.getElementById('cart-items');
+    const orderTotalElement = document.getElementById('order-total');
+    cartList.innerHTML = '';
+
+   
+}
