eac287723290aa63d83dffa746126cc7637d9da8
Rachanashivarkar <rachanashivarkar4113@gmail.com>
2024-07-22 08:04:15 +0530

    changes made in home.js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footerText = document.getElementById('footer-text');
     footerText.style.position = 'relative';
-    footerText.style.top = '-5px'; // Adjust the value as needed
+    footerText.style.top = '-5px'; 
 });
 
 
