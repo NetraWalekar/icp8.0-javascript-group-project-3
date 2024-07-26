c746c4271f0ee4c4402ed638f32589383a026c53
Rachanashivarkar <rachanashivarkar4113@gmail.com>
2024-07-26 14:44:16 +0530

    js added to image slider

--- js/home.js
+++ js/home.js
@@ -1,7 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footerText = document.getElementById('footer-text');
-    footerText.style.position = 'relative';
-    footerText.style.top = '-5px'; 
+let images = [
+    "./images/icons/home-logo/head-img.jpg",
+    "./images/icons/home-logo/img5.jpg",
+    "./images/icons/home-logo/img6.jpeg",
+    "./images/icons/home-logo/img7.jpeg"
+];
+
+let imgIndex = 0;
+
+function showImage(index) {
+    const imgElement = document.getElementById('head-img');
+    imgElement.src = images[index];
+}
+
+function prev() {
+    if (imgIndex === 0) {
+        imgIndex = images.length - 1;
+    } else {
+        imgIndex--;
+    }
+    showImage(imgIndex);
+}
+
+function next() {
+    if (imgIndex === images.length - 1) {
+        imgIndex = 0;
+    } else {
+        imgIndex++;
+    }
+    showImage(imgIndex);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showImage(imgIndex);
 });
-
-
